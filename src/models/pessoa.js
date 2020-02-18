81bc466555e4c9bc994604c7f95f5700700ac101
Dionleno <dhonlennon20@gmail.com>
2020-02-17 22:24:23 -0300

    Inserindo exemplo de array de objetos

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -6,7 +6,8 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     nome: String,
     cpf: String,
-    dataCadastro: { type: Date, default: Date.now }
+    dataCadastro: { type: Date, default: Date.now },
+    telefone: [String] // array de objetos
 });
 
 module.exports = mongoose.model('Pessoa', schema);

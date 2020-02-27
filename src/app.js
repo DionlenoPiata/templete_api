11744a2e032b423b29f61e104a54fad68d7959a6
DionlenoPiata <39806316+DionlenoPiata@users.noreply.github.com>
2020-02-27 14:51:19 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -47,6 +47,14 @@ app.use(bodyParser.json()); // converte o que chega para json
 app.use(bodyParser.urlencoded({ extended: false })); // codificar as urls
 /* -------------------------------------------------------------------*/
 
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // ROTA ESTATICAS DA APLICAÇÃO
 /* -------------------------------------------------------------------*/
 app.use('/', indexRoute);
